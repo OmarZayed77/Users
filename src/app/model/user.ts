561c6dd1dfb8a7617bd0300683659dadd1228754
OmarZayed77 <zayedomarr@gmail.com>
2019-05-27 21:33:00 +0200

    Add Folder structure, user model and Edit README file

---
+++ src/app/model/user.ts
@@ -0,0 +1,11 @@
+export default interface User
+{
+    id? : string;
+    fName? : string;
+    lName? : string;
+    email? : string;
+    address? : string;
+    city?: string;
+    state? : string;
+    gender? : Gender;
+}
